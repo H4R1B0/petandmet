5bcfab90d648b4cd60b21a866e35ebea10201a82
최시환 <pum005@naver.com>
2023-07-31 14:01:09 +0900

    feat: 보호소 물품 API 추가

---
+++ BackEnd/src/main/java/com/ssafy/petandmet/dto/centerItem/CreateCenterItemResponse.java
@@ -0,0 +1,12 @@
+package com.ssafy.petandmet.dto.centerItem;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CreateCenterItemResponse {
+
+    private String message;
+    private String status;
+}
