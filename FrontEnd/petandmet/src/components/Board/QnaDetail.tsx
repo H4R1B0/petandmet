6912cba33633d3184bec0f8360c02a20af2e7668
wndjs8225 <wndjs8225@naver.com>
2023-08-04 13:48:59 +0900

    feat     : 댓글 목록 컴포넌트, 질문 게시글 댓글 기능 추가

--- FrontEnd/petandmet/src/components/Board/QnaDetail.tsx
+++ FrontEnd/petandmet/src/components/Board/QnaDetail.tsx
@@ -1,7 +1,21 @@
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography, TextField, Grid } from "@mui/material";
 import BoardDetail from "../../containers/components/BoardDetail"
-
+import {useState} from 'react'
+import CommentList from "../../containers/components/CommentList";
 function QnaDetail(){
+
+    const [comment, setComment] = useState<string>('')
+    const [commentList, setCommentList] = useState<string[]>([])
+    
+    const handleCommentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setComment(event.target.value);
+    };
+
+    const handleCommentSubmit = () => {
+        setCommentList((prevComments) => [...prevComments, comment]);
+        setComment("");
+    };
+
     return(
         <>
         <Container>
@@ -11,6 +25,29 @@ function QnaDetail(){
                 </Typography>
             </div>
             <BoardDetail></BoardDetail>
+
+            <Container sx={{width : '80%'}}>
+                <CommentList commentList={commentList}></CommentList>
+            </Container>
+
+            <Grid container justifyContent='space-evenly' alignItems="center">
+                <Grid item xs={6}>
+                    <TextField
+                        id="comment"
+                        placeholder='댓글을 입력하세요'
+                        sx={{ width: '100%' }}
+                        onChange={handleCommentChange}
+                        value={comment}
+                    />
+                </Grid>
+                <Grid item xs={1}>
+                    <Button sx={{backgroundColor : '#1E90FF',
+                                "&:hover":{backgroundColor :'#4FC3F7'},
+                                color : 'black'}}
+                            onClick={handleCommentSubmit}
+                                >등록</Button>
+                </Grid>
+            </Grid>
 
             <Box sx={{textAlign  : 'right', width : '88%'}}>
                 <Button sx={{backgroundColor : '#1E90FF', "&:hover":{backgroundColor :'#4FC3F7'}, color : 'black', marginRight:'5px'}}>수정</Button>
---
+++ FrontEnd/petandmet/src/containers/components/CommentList.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material';
+interface CommentListProps{
+    commentList : string[];
+}
+function CommentList(props: CommentListProps) {
+  return (
+ 
+    <List sx={{ width: '80%', bgcolor: 'background.paper' }}>
+      {props.commentList.map((comment, index) => (
+          <React.Fragment key={index}>
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="User" src="/static/images/avatar/1.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="User ID"
+              secondary={
+                  <>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                    >
+                    {comment} {/* 각각의 댓글을 표시 */}
+                  </Typography>
+                </>
+              }
+              />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+        </React.Fragment>
+      ))}
+    </List>
+  );
+}
+
+export default CommentList
+export{}
