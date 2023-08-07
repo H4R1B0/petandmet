180cd96453f3823e9bb447278612073a29254137
이주언 <wndjs8225@naver.com>
2023-08-07 10:29:46 +0900

    feat: 컴포넌트 라우터 적용

--- FrontEnd/petandmet/src/App.tsx
+++ FrontEnd/petandmet/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
-import Navbar from './components/Main/Navbar'
 import Router from './routes/Router'
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
       <Router></Router>
     </div>
   )
