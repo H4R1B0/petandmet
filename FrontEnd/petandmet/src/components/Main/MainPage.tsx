08a7a80559e8879ce6a45436782cbb7fb7f02801
wndjs8225 <wndjs8225@naver.com>
2023-08-08 16:11:11 +0900

    refactor : 메인, 라이브리스트, 보호동물리스트 수정

--- FrontEnd/petandmet/src/components/Main/MainPage.tsx
+++ FrontEnd/petandmet/src/components/Main/MainPage.tsx
@@ -8,21 +8,26 @@ import AnimalList from 'components/Animal/AnimalList'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
+interface CustomButtonProps {
+  isActive: boolean;
+}
+
 const btn = ['라이브', '보호동물', '봉사']
-const CustomButton = styled(Button)(({ theme }) => ({
-  backgroundColor: '#FFA629',
+const CustomButton = styled(Button)(({ isActive }: CustomButtonProps) => ({
+  backgroundColor: isActive ? '#FF6F26'  : '#FFA629',
   color: 'white',
   padding: '10px 20px',
   borderRadius: '5px',
   boxShadow: 'none',
   '&:hover': {
-    backgroundColor: 'orange',
+    backgroundColor: '#FF6F26',
   },
   margin: '5px',
 }))
 
 function MainPage() {
   const [channel, setChannel] = useState(0)
+  const [isActive, setIsActive] = useState([true, false, false]);
   let navigate = useNavigate()
   const goToLiveList = () => {
     navigate('/livelist')
@@ -33,6 +38,12 @@ function MainPage() {
   const goToVolunteer = () => {
     navigate('/volunteer')
   }
+  const handleButtonClick = (idx: number) => {
+    setChannel(idx);
+    const newIsActive = isActive.map((_, i) => i === idx);
+    setIsActive(newIsActive);
+  };
+
   return (
     <>
       <CssBaseline />
@@ -52,36 +63,34 @@ function MainPage() {
           }}
         >
           {btn.map((b, idx) => (
-            <CustomButton key={idx} onClick={() => setChannel(idx)}>
+            <CustomButton
+              key={idx}
+              isActive={isActive[idx]}
+              onClick={() => handleButtonClick(idx)}>
               {b}
             </CustomButton>
           ))}
         </Box>
 
-        <Box
-          sx={{
-            mt: 1,
-            display: 'grid',
-            gridTemplateColumns: 'repeat(4, 1fr)', // 이 부분을 추가하여 카드를 자동으로 정렬합니다.
-            gap: '8px', // 카드 간 간격 설정
-            height: '95%',
-          }}
-        >
-          {channel === 1 ? <AnimalList></AnimalList> : <MainLive></MainLive>}
+        <Box>
+          {
+          channel === 1 
+          ? <AnimalList num={8}/>
+          :<MainLive num={8}/>}
         </Box>
 
         <CustomButton
+          isActive={isActive[channel]}
           onClick={
-            channel === 1
+              channel === 1
               ? goToAnimalList
               : channel === 2
               ? goToVolunteer
               : goToLiveList
           }
         >
-          더보기
+          {btn[channel]} 더보기
         </CustomButton>
-        {/* 더보기 눌렀을 시 라이브 리스트, 보호동물 리스트 페이지로 각각 이동 추후개발 */}
       </Container>
     </>
   )
