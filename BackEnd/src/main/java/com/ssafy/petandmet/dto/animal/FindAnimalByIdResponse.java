05e550ff7b2539f0f59a9790ee303ae6f387f4c1
최시환 <pum005@naver.com>
2023-08-09 15:07:50 +0900

    fix : FindAnimalByIdResponse dto 수정

--- BackEnd/src/main/java/com/ssafy/petandmet/dto/animal/FindAnimalByIdResponse.java
+++ BackEnd/src/main/java/com/ssafy/petandmet/dto/animal/FindAnimalByIdResponse.java
@@ -1,5 +1,6 @@
 package com.ssafy.petandmet.dto.animal;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,25 +16,27 @@ public class FindAnimalByIdResponse {
     private int age;
     private String specie;
     private String breed;
+    @JsonProperty("find_place")
     private String findPlace;
+    @JsonProperty("center_uuid")
     private String centerUuid;
+    @JsonProperty("enter_date")
     private LocalDateTime enterDate;
+    @JsonProperty("adoption_status")
     private AdoptionStatus adoptionStatus;
-
     private Gender gender;
-
+    @JsonProperty("notice_date")
     private String noticeDate;
-
-    private LocalDateTime adotionStartDate;
-
+    @JsonProperty("adoption_start_date")
+    private LocalDateTime adoptionStartDate;
     private CharacterType character;
-
+    @JsonProperty("enter_age")
     private int enterAge;
-
+    @JsonProperty("photo_url")
     private String photoUrl;
 
     @Builder
-    public FindAnimalByIdResponse(String photoUrl, int enterAge, CharacterType character, String message, String status, String name, int age, String specie, String breed, String findPlace, String centerUuid, LocalDateTime enterDate, AdoptionStatus adoptionStatus, Gender gender, String noticeDate, LocalDateTime adotionStartDate) {
+    public FindAnimalByIdResponse(String photoUrl, int enterAge, CharacterType character, String message, String status, String name, int age, String specie, String breed, String findPlace, String centerUuid, LocalDateTime enterDate, AdoptionStatus adoptionStatus, Gender gender, String noticeDate, LocalDateTime adoptionStartDate) {
         this.photoUrl = photoUrl;
         this.enterAge = enterAge;
         this.character = character;
@@ -49,6 +52,6 @@ public class FindAnimalByIdResponse {
         this.adoptionStatus = adoptionStatus;
         this.gender = gender;
         this.noticeDate = noticeDate;
-        this.adotionStartDate = adotionStartDate;
+        this.adoptionStartDate = adoptionStartDate;
     }
 }
