304d50cf0b1f55de3b3812b7aa7b01f787ce16e2
이주언 <wndjs8225@naver.com>
2023-08-10 17:45:33 +0900

    #115 feat create donateitem

--- FrontEnd/petandmet/src/components/Item/ItemList.tsx
+++ FrontEnd/petandmet/src/components/Item/ItemList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {
   Grid,
   Box,
@@ -11,51 +11,150 @@ import {
 } from '@mui/material'
 import Select, { SelectChangeEvent } from '@mui/material/Select'
 import pay from 'images/kakaopay.png'
+import CenterDataList from 'hooks/Center/CenterMutation';
+import axios from 'axios';
+import { domain } from 'hooks/customQueryClient';
+import { useNavigate } from 'react-router-dom';
 
-const data = [
-  {
-    src: pay,
-    title: 'Don Diablo @ Tomorrowland Main Stage 2019 | Official…',
-    price: 'Don Diablo',
-    Center: '396k Center',
-  },
-  {
-    src: pay,
-    title: 'Queen - Greatest Hits',
-    price: 'Queen Official',
-    Center: '40M Center',
-  },
-  {
-    src: pay,
-    title: 'Calvin Harris, Sam Smith - Promises (Official Video)',
-    price: 'Calvin Harris',
-    Center: '130M Center',
-  },
-]
-
-interface LoadProps {
-  loading?: boolean
+interface CenterItem {
+  center_item_id: number;
+  center_uuid: string;
+  item_name: string;
+  item_target_price: number;
+  item_url: string | null;
 }
-const centers = ['A보호소', 'B보호소', 'C보호소']
 
-function List(props: LoadProps) {
-  const { loading = false } = props
+function List() {
   const [center, setCenter] = useState('')
+  const [uid, setUid] = useState('')
+  const navigate = useNavigate()
+
+  const goToItemDetail = (item : CenterItem) => {
+    navigate(`/donate/item/${uid}`, {state : item})
+  }
+
   const handleChange = (event: SelectChangeEvent) => {
     setCenter(event.target.value)
   }
+  
+  const [centers, setCenters] = useState<string[]>([]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const centersData = await CenterDataList();
+      setCenters(centersData);
+      console.log(centersData)
+    };
+    fetchData();
+  }, []);
+  
 
-  return (
-    <Container>
-      <div style={{ padding: 20 }}>
+  const [centerItem, setCenterItem] = useState<CenterItem[]>([]);
+
+  const CenterItemList =async () => {
+    try{
+      const res = await axios.get(`${domain}/center/item?uuid=${uid}`)
+      console.log(res)
+      const centerItem = res.data.response.centerItems
+      console.log(centerItem)
+      return centerItem
+    } catch(error){
+      console.log(error)
+      return [];
+    }
+  }
+  
+  useEffect(() => {
+    const fetchItem =async () => {
+      const centersItem = await CenterItemList()
+      setCenterItem(centersItem)
+      console.log(centerItem)
+    }
+    fetchItem()
+  },[uid])
+
+  if (centerItem.length !== 0) {
+    return (
+      <Container>
+        <div style={{ padding: 20 }}>
+          <Typography
+            variant="h4"
+            style={{ color: '#FFA629', fontWeight: 'bold' }}
+            >
+            후원 물품 목록
+          </Typography>
+        </div>
+
+        <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+          <FormControl sx={{ width: '25%' }}>
+            <InputLabel id="demo-simple-select-label">보호소</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={center}
+              label="보호소"
+              onChange={handleChange}
+              >
+              {centers.map((cent:any) => (
+                <MenuItem value={cent.uuid} onClick={() => setUid(cent.uuid)}>{cent.name}</MenuItem>
+                ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        {/* 보호소 아이디에 해당하는 물품 map을 통해 보여주기 */}
+
+        <Grid container spacing={2}>
+          {centerItem.map((item, index) => (
+            <Grid key={index} item xs={12} sm={6} md={3}>
+              <Box sx={{ width: '100%', my: 5 }}>
+                {item ? (
+                  <img
+                  style={{ width: '100%', height: 200, objectFit: 'cover' }}
+                  alt={pay}
+                  src={pay}
+                  onClick={() =>{goToItemDetail(item)}}
+                  />
+                  ) : (
+                    <Skeleton variant="rectangular" width="100%" height={200} />
+                    )}
+                {item ? (
+                  <Box sx={{ pr: 2 }}>
+                    <Typography gutterBottom variant="body2">
+                      {item.item_name}
+                    </Typography>
+                    <Typography
+                      display="block"
+                      variant="caption"
+                      color="text.secondary"
+                      >
+                      {item.item_target_price}
+                    </Typography>
+                  </Box>
+                ) : (
+                  <Box sx={{ pt: 0.5 }}>
+                    <Skeleton />
+                    <Skeleton width="60%" />
+                  </Box>
+                )}
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    )
+  }
+else{
+  return(
+    <>
+    <div style={{ padding: 20 }}>
         <Typography
           variant="h4"
           style={{ color: '#FFA629', fontWeight: 'bold' }}
-        >
+          >
           후원 물품 목록
         </Typography>
       </div>
-
       <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
         <FormControl sx={{ width: '25%' }}>
           <InputLabel id="demo-simple-select-label">보호소</InputLabel>
@@ -65,59 +164,19 @@ function List(props: LoadProps) {
             value={center}
             label="보호소"
             onChange={handleChange}
-          >
-            {centers.map((cent, idx) => (
-              <MenuItem value={idx}>{cent}</MenuItem>
-            ))}
+            >
+            {centers.map((cent:any) => (
+              <MenuItem value={cent.uuid} onClick={() => setUid(cent.uuid)}>{cent.name}</MenuItem>
+              ))}
           </Select>
         </FormControl>
       </div>
 
-      {/* 보호소 아이디에 해당하는 물품 map을 통해 보여주기 */}
-
-      <Grid container spacing={2}>
-        {(loading ? Array.from(new Array(3)) : data).map((item, index) => (
-          <Grid key={index} item xs={12} sm={6} md={3}>
-            <Box sx={{ width: '100%', my: 5 }}>
-              {item ? (
-                <img
-                  style={{ width: '100%', height: 200, objectFit: 'cover' }}
-                  alt={item.title}
-                  src={item.src}
-                />
-              ) : (
-                <Skeleton variant="rectangular" width="100%" height={200} />
-              )}
-              {item ? (
-                <Box sx={{ pr: 2 }}>
-                  <Typography gutterBottom variant="body2">
-                    {item.title}
-                  </Typography>
-                  <Typography
-                    display="block"
-                    variant="caption"
-                    color="text.secondary"
-                  >
-                    {item.price}
-                  </Typography>
-                  <Typography variant="caption" color="text.secondary">
-                    {`${item.Center}`}
-                  </Typography>
-                </Box>
-              ) : (
-                <Box sx={{ pt: 0.5 }}>
-                  <Skeleton />
-                  <Skeleton width="60%" />
-                </Box>
-              )}
-            </Box>
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+      <h1>등록된 후원 물품이 없습니다.</h1>
+    </>
   )
 }
-
+}
 function ItemList() {
   return (
     <Box sx={{ overflow: 'hidden' }}>
