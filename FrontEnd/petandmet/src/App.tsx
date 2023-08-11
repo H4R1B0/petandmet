02f3ca545b9616544adab9de6449618fe70002b1
황준혁 <hyug0819@gmail.com>
2023-08-11 14:04:46 +0900

    feat: login 구현

--- FrontEnd/petandmet/src/App.tsx
+++ FrontEnd/petandmet/src/App.tsx
@@ -1,9 +1,23 @@
 import './App.css'
 import Router from 'routes/Router'
+import { ToastContainer } from 'react-toastify'
 function App() {
   return (
-    <div className="App">
+    <div className="App h-screen">
       <Router></Router>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        limit={5}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover
+        theme="colored"
+      />
     </div>
   )
 }
