6cdbedbaf53bc5251a78a88b428a0044e9a1b8b4
황준혁 <hyug0819@gmail.com>
2023-08-11 09:48:52 +0900

    feat: axios 관련 기능

--- FrontEnd/petandmet/src/hooks/customQueryClient.tsx
+++ FrontEnd/petandmet/src/hooks/customQueryClient.tsx
@@ -3,7 +3,9 @@ import { QueryClient } from 'react-query'
 const domain = 'https://i9b302.p.ssafy.io/api/v1'
 const queryClient = new QueryClient({
   defaultOptions: {
-    queries: {},
+    queries: {
+      retry: 2,
+    },
     mutations: {},
   },
 })
