ad8da80f37bcd91cc6ab24256ecf65d5dfd7ed46
최시환 <pum005@naver.com>
2023-08-11 11:21:37 +0900

    fix: AnimalFrindlinessResponse  dto 수정

--- BackEnd/src/main/java/com/ssafy/petandmet/dto/user/AnimalFrindlinessResponse.java
+++ BackEnd/src/main/java/com/ssafy/petandmet/dto/user/AnimalFrindlinessResponse.java
@@ -1,12 +1,12 @@
 package com.ssafy.petandmet.dto.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class AnimalFrindlinessResponse {
+    private int status;
     private Long percent;
 
-    public AnimalFrindlinessResponse(Long percent) {
-        this.percent = percent;
-    }
 }
