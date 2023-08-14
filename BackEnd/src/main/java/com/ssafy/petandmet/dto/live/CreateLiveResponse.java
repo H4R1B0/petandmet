e7588093629ba262e0e5c4dcc5030dbe06125dcc
최시환 <pum005@naver.com>
2023-08-14 10:21:27 +0900

    Update CreateLiveResponse.java

--- BackEnd/src/main/java/com/ssafy/petandmet/dto/live/CreateLiveResponse.java
+++ BackEnd/src/main/java/com/ssafy/petandmet/dto/live/CreateLiveResponse.java
@@ -1,5 +1,6 @@
 package com.ssafy.petandmet.dto.live;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,4 +10,6 @@ public class CreateLiveResponse {
 
     private String message;
     private String status;
+    @JsonProperty("live_id")
+    private Long liveId;
 }
