bde964fdbe75ea9595d954de3f19a8d91ab2aced
황준혁 <hyug0819@gmail.com>
2023-08-18 03:07:44 +0900

    refactor: path 수정

--- FrontEnd/petandmet/src/routes/Center.tsx
+++ FrontEnd/petandmet/src/routes/Center.tsx
@@ -3,27 +3,21 @@ import ItemRegister from 'components/Center/CenterItemEnroll'
 import UpdateCenterItem from 'components/Center/update/UpdateItem'
 import CenterPage from 'components/Center/CenterPage'
 import UpdateCenter from 'components/Center/update/UpdateCenter'
-import CenterWalk from 'components/Volunteer/Walk/walkCenter'
-import WalkUpdate from 'containers/components/WalkUpdate'
-function Center(){
-    return(
-        <>
-        <Routes>
-            <Route path="/" element={<CenterPage />} />
-            <Route path="/item/*">
-                <Route path="enroll" element={<ItemRegister/>}/>
-                <Route path="update" element={<UpdateCenterItem/>}/>
-            </Route>
-            <Route path="/center/*">
-                <Route path="update" element={<UpdateCenter/>}/>
-            </Route>
-            <Route path="/walk/*">
-                <Route path="" element={<CenterWalk/>}/>
-                <Route path="update" element={<WalkUpdate/>}/>
-            </Route>
-        </Routes>
-        </>
-    )
+import WalkUpdate from 'components/Center/update/WalkUpdate'
+function Center() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<CenterPage />} />
+        <Route path="/item/*">
+          <Route path="enroll" element={<ItemRegister />} />
+          <Route path="update" element={<UpdateCenterItem />} />
+        </Route>
+        <Route path="update" element={<UpdateCenter />} />
+        <Route path="walk" element={<WalkUpdate />} />
+      </Routes>
+    </>
+  )
 }
 
 export default Center
